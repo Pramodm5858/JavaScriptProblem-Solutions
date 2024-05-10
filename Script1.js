a4fd3327aff44d7814de7d122288f2ee39a4c9f7
Pramodm5858 <pramodm5858@gmail.com>
2024-05-10 05:57:47 +0530

    All questions & solutions

---
+++ Script1.js
@@ -0,0 +1,89 @@
+console.log("Hello World1");
+
+//JavaScript problems & solutions
+
+//1) // alert("Hello World")
+        // console.log("Hello world2 ");
+        // document.write("Hello World")
+        
+//2)Add of two number
+   //static
+        // let a = 5;
+        // let b = 6;
+
+        // let c = a + b;
+        //As we know + is used to add number & also concatination of string;
+        //Imp whenever we are going to use +(addition) then we have to use as parseInt(value).as it converts from string to numbers 
+
+        //console.log("The sum of " +a+ " & " +b+ " is = " +c);
+
+
+   //dynamic calculation
+        //let d = prompt("Please enter first number ", "");
+        //let e = prompt("Please enter second number ", "");
+            //To convert from string to number we use parseInt() 
+        //let f = parseInt(d) + parseInt(e);
+        //console.log(f);
+
+//3) To find squareroot of number in js by using Math.sqrt()
+        //let g = prompt("Enter the number ","")
+        //let h = Math.sqrt(g)
+        //let i = Math.sqrt("Hello" or -6 ) O/p => NaN 
+
+        //console.log(i);
+
+//4) Find Area of triangle
+        // let base = prompt("Enter base of a triangle ","");
+        // let height = prompt("Enter height of triangle ","");
+
+        // let area = ( parseInt(base) * parseInt(height))/2
+
+        // console.log("base is " +base+ " height is " +height+ " area is " +area);
+
+        // let base1 = prompt("Enter base of a triangle ","");
+        // let height1 = prompt("Enter height of triangle ","");
+
+        // let area1 = ( base1 * height1)/2
+
+        // console.log("base is " +base1+ " height is " +height1+ " area is " +area1);
+
+
+
+//5) Find area of regular triangular s=(a+b+c)/2 & Area= squareroot s(s-a)*(s-b)*(s-c)
+
+        // let a = 5;
+        // let b = 10;
+        // let c = 10;
+
+        // let s = (a + b + c) / 2;
+
+        // console.log(s);
+
+        // let temp = s * (s - a) * (s - b) * (s - c)
+        // console.log(temp);
+        // let area = Math.sqrt(temp)
+
+        // console.log(area);
+
+// 6) Swapping of two values
+
+                //  let a = 5;
+                //  let b = 6;
+                //  let temp;
+        
+                //  temp=a;
+                //  a=b;
+                //  b=temp
+                 
+                //  console.log("a is " +a+ " b is " +b);
+        
+                //  let p = parseInt(prompt("Enter the p number"));
+                //  let q = parseInt(prompt("Enter the q number"));     
+                //     console.log("p is " +p+ " q is " +q);
+        
+                //  p = p+q 
+                //  q = p-q
+                //  p = p -q
+        
+                //     console.log("p is " +p+ " q is " +q);
+
